654565586d3b58cae16d26765a4830dd21014839
alimehdi-tech <mmalimehdi111@gmail.com>
2024-11-05 21:31:48 +0500

    Update Media.jsx

--- src/component/shared/Media.jsx
+++ src/component/shared/Media.jsx
@@ -18,28 +18,8 @@ const Media = () => {
         <p className="text-gray-500 text-sm">Google</p>
       </button>
 
-      <div className="flex items-center">
-        <span className="w-0.5 h-8 bg-gray-300"></span>
-      </div>
-
-      <Link to="#" className="flex items-center m-2">
-        <p className="text-blue-500 flex items-center text-lg p-2 font-extrabold">
-          <FaFacebookSquare className="text-2xl " />
-          acebook
-        </p>
-      </Link>
-
-      <div className="flex items-center">
-        <span className="w-0.5 h-8 bg-gray-300"></span>
-      </div>
-
-      <Link
-        to="#"
-        className="flex items-center linkedin m-2 text-2xl p-2 font-bold"
-      >
-        linked
-        <FaLinkedin />
-      </Link>
+      
+      
     </div>
   );
 };
